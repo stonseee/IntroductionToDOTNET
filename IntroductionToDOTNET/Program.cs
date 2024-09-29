742db6c0d6e0fa0328d990aaa2ed31ba4bf54c1c
stonseee <stonseee@STONSEEE>
2024-09-29 19:16:41 +0300

    Geometria

--- IntroductionToDOTNET/Program.cs
+++ IntroductionToDOTNET/Program.cs
@@ -1,5 +1,6 @@
 ﻿//#define CONSOLE_CLASS
 //#define STRING_OPERATIONS
+//#define HARD_CHESS
 using System;
 using System.Collections.Generic;
 using System.Diagnostics.Eventing.Reader;
@@ -27,7 +28,7 @@ namespace IntroductionToDOTNET
 
             Console.WriteLine(delimiter);
 
-            #region ConsoleWindowParameters
+			#region ConsoleWindowParameters
             Console.WriteLine("Позиция окна консоли: " + Console.WindowLeft + " знакопозиций");
             Console.WriteLine("Позиция окна консоли: " + Console.WindowTop + " знакопозиций");
 
@@ -39,7 +40,7 @@ namespace IntroductionToDOTNET
             Console.WriteLine("Ширина буфера консоли: " + Console.BufferWidth + " знакопозиций");
             Console.WriteLine("Высота буфера консоли: " + Console.BufferHeight + " знакопозиций");
 
-            #endregion
+			#endregion
 #endif
 #if STRING_OPERATIONS
             Console.Write("Введите ваше имя: ");
@@ -56,44 +57,157 @@ namespace IntroductionToDOTNET
             Console.WriteLine($"{first_name} {last_name} {age}");                         //интерполяция строк
 #endif
 
+#if HARD_CHESS
             int scale = 4;
             int size = 3;
             char a = '*';
             char b = '-';
 
-            for(int m = 0; m < scale * 2; m++)
+            for (int m = 0; m < scale * 2; m++)
             {
-				for (int n = 0; n < size; ++n)
-				{
-					for (int i = 0; i < scale; i++)
-					{
-						for (int j = 0; j < size; j++)
-							Console.Write(a + " ");
-
-						for (int j = 0; j < size; j++)
-							Console.Write(b + " ");
-
-						if (i == scale - 1)
-							Console.WriteLine("");
-					}
-				}
-                if(m % 2 == 0)
+                for (int n = 0; n < size; ++n)
                 {
-					a = '-';
-					b = '*';
-				}				
+                    for (int i = 0; i < scale; i++)
+                    {
+                        for (int j = 0; j < size; j++)
+                            Console.Write(a + " ");
+
+                        for (int j = 0; j < size; j++)
+                            Console.Write(b + " ");
+
+                        if (i == scale - 1)
+                            Console.WriteLine("");
+                    }
+                }
+                if (m % 2 == 0)
+                {
+                    a = '-';
+                    b = '*';
+                }
                 else
                 {
-					a = '*';
-					b = '-';
-				}					
-			}          
-            
+                    a = '*';
+                    b = '-';
+                }
+            }       
+#endif
 
+			int scale = 5;
+            char sym = '*';
+            char fym = '*';
 
+            for(int i = 0; i < scale; ++i)
+            {
+                for(int j = 0; j < scale; ++j)
+                {
+                    if (j < scale - 1)
+                        Console.Write("* ");
+                    else
+                        Console.WriteLine("* ");
+                }
+            }
 
+			Console.WriteLine("");
+			Console.WriteLine("");
 
+			for (int i = 0; i < scale; ++i)
+			{
+				for (int j = 0; j < scale; ++j)
+				{
+                    if (i >= j)
+                        sym = '*';
+                    else
+                        sym = ' ';
 
+					if (j < scale - 1)
+						Console.Write(sym + " ");
+					else
+						Console.WriteLine(sym + " ");
+				}
+			}
+
+			Console.WriteLine("");
+			Console.WriteLine("");
+
+			for (int i = 0; i < scale; ++i)
+			{
+				for (int j = 0; j < scale; ++j)
+				{
+					if (j < scale - i)
+						sym = '*';
+					else
+						sym = ' ';
+
+					if (j < scale - 1)
+						Console.Write(sym + " ");
+					else
+						Console.WriteLine(sym + " ");
+				}
+			}
+
+			Console.WriteLine("");
+			Console.WriteLine("");
+
+			for (int i = 0; i < scale; ++i)
+			{
+				for (int j = 0; j < scale; ++j)
+				{
+					if (i <= j)
+						sym = '*';
+					else
+						sym = ' ';
+
+					if (j < scale - 1)
+						Console.Write(sym + " ");
+					else
+						Console.WriteLine(sym + " ");
+				}
+			}
+
+			Console.WriteLine("");
+			Console.WriteLine("");
+
+			for (int i = 0; i < scale; ++i)
+			{
+				for (int j = 0; j < scale; ++j)
+				{
+					if (j >= scale - i - 1)
+						sym = '*';
+					else
+						sym = ' ';
+
+					if (j < scale - 1)
+						Console.Write(sym + " ");
+					else
+						Console.WriteLine(sym + " ");
+				}
+			}
+
+			Console.WriteLine("");
+			Console.WriteLine("");
+
+			scale *= 2;
+
+			for (int i = 0; i < scale; ++i)
+			{
+				for (int j = 0; j < scale; ++j)
+				{					
+					if (j == scale - (i - scale / 2) - 1 || i == scale / 2 - j - 1)						
+						sym = '/';
+					else if (j == scale / 2 + i || i == scale / 2 + j)
+						sym = '\\';					
+					else
+						sym = ' ';
+
+					if (j < scale - 1)
+						Console.Write(sym);
+					else
+						Console.WriteLine(sym);
+				}
+			}
+
+			Console.WriteLine("");
+			Console.WriteLine("");
 		}
 	}
 }
