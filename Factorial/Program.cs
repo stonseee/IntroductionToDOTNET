5537f7b46281583a2244b0a058079cd7729d5310
stonseee <stonseee@STONSEEE>
2024-10-02 18:33:45 +0300

    Factorial DONE

---
+++ Factorial/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Factorial
+{
+	internal class Program
+	{
+		static void Main(string[] args)
+		{
+			decimal num = 0;
+			decimal result = 1;
+			string buffer = Console.ReadLine();
+			num = Convert.ToDecimal(buffer);
+
+			for(int i = 0; i < num; i++)
+			{		
+				result *= i + 1;
+			}
+
+			Console.WriteLine("result " + result);	
+			
+		}
+	}
+}
