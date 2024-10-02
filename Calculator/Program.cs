0fcd663bddbc2de62cc37ee945859e275a5b2c6d
stonseee <stonseee@STONSEEE>
2024-10-02 18:05:57 +0300

    Calculator DONE

---
+++ Calculator/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Calculator
+{
+	internal class Program
+	{
+		static void Main(string[] args)
+		{
+			char[] delimiters = { '+', '-', '*', '/'};
+			char operation = ' ';
+			double result = 0;
+			
+			string input = Console.ReadLine();			
+
+			for(int i = 0; i < input.Length; i++)
+			{				
+				if (input[i] == '+' || input[i] == '-' || input[i] == '*' || input[i] == '/')
+				{
+					operation = input[i];
+				}
+			}
+
+			string[] buffer = input.Split(delimiters);
+
+			if(operation == '+')
+			result = Convert.ToDouble(buffer[0]) + Convert.ToDouble(buffer[1]);
+			if (operation == '-')
+				result = Convert.ToDouble(buffer[0]) - Convert.ToDouble(buffer[1]);
+			if (operation == '*')
+				result = Convert.ToDouble(buffer[0]) * Convert.ToDouble(buffer[1]);
+			if (operation == '/')
+				result = Convert.ToDouble(buffer[0]) / Convert.ToDouble(buffer[1]);
+
+			Console.WriteLine("=" + result);
+		}
+	}
+}
